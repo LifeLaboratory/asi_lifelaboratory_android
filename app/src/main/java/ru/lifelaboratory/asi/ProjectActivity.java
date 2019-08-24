0be4bf12f918093c0a52c4a715e49ee5f077fc18
Lioran <wersil@mail.ru>
2019-08-24 23:27:05 +0700

    Наработки по странице проекты

--- app/src/main/java/ru/lifelaboratory/asi/ProjectActivity.java
+++ app/src/main/java/ru/lifelaboratory/asi/ProjectActivity.java
@@ -2,13 +2,19 @@ package ru.lifelaboratory.asi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import ru.lifelaboratory.asi.adapter.ProjectAdapter;
+import ru.lifelaboratory.asi.entity.Category;
 import ru.lifelaboratory.asi.entity.Project;
 import ru.lifelaboratory.asi.service.ProjectService;
 import ru.lifelaboratory.asi.utils.Constants;
@@ -25,7 +31,21 @@ public class ProjectActivity extends Activity {
 
         Retrofit retrofit = new Retrofit.Builder().baseUrl(Constants.SERVER_URL).
                 addConverterFactory(GsonConverterFactory.create()).build();
-        ProjectService allProject = retrofit.create(ProjectService.class);
+        ProjectService projectService = retrofit.create(ProjectService.class);
+        //todo добавить фичу - фильтрация по категориям
+        Call<ArrayList<Project>> getAllProject = projectService.getAllProject(new ArrayList<Category>());
+        getAllProject.enqueue(new Callback<ArrayList<Project>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Project>> call, Response<ArrayList<Project>> response) {
+                projects.addAll(response.body());
+                Log.d(Constants.LOG_TAG, "Найс запрос, проекты в хате");
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Project>> call, Throwable t) {
+                Log.e(Constants.LOG_TAG, "ДЖИЗУС КРАЙЗИС, Проекты потеряны");
+            }
+        });
 
         projectAdapter = new ProjectAdapter(this, projects);
 
--- app/src/main/java/ru/lifelaboratory/asi/service/ProjectService.java
+++ app/src/main/java/ru/lifelaboratory/asi/service/ProjectService.java
@@ -1,6 +1,19 @@
 package ru.lifelaboratory.asi.service;
 
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import ru.lifelaboratory.asi.entity.Category;
+import ru.lifelaboratory.asi.entity.Project;
+
 public interface ProjectService {
 
+    @POST("project/filter")
+    Call<ArrayList<Project>> getAllProject(@Body ArrayList<Category> categories);
+
+    @POST("project/filter")
+    Call<ArrayList<Project>> getAllProject(@Body Integer idUser);
 
 }
